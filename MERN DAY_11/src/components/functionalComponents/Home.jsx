a8df61ebd8a5e60a386c9d7d1c708ff4d0d943cb
umasankari220 <umasankari.m2024csecs@sece.ac.in>
2025-12-17 10:52:55 +0530

    Add files via upload

---
+++ MERN DAY_11/src/components/functionalComponents/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div style={{ padding: '40px 20px' }}>
+            <h1>Home Page</h1>
+            <p>Welcome to the Home Page</p>
+            <p>This is your React application homepage!</p>
+        </div>
+    )
+}
+export default Home;
