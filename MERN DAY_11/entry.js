2814c1ab4432e74a75a250b866e9c51a0d71302c
umasankari220 <umasankari.m2024csecs@sece.ac.in>
2025-12-17 11:33:45 +0530

    Change MongoDB connection database name to 'MERN'
    Updated MongoDB connection string and improved code formatting.

--- MERN DAY_11/entry.js
+++ MERN DAY_11/entry.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors())
 
 mdb
-  .connect("mongodb://localhost:27017/seceDec2025")
+  .connect("mongodb://localhost:27017/MERN")
   .then(() => console.log("MongoDB Connection Successful"))
   .catch((err) => console.log("MongoDB Connection Unsuccessful", err));
 
